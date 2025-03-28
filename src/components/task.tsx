fe3a40be28c30ec283c15b94fe7f2ba3de50095b
Vojtěch Pašek <pasekvoj@fit.cvut.cz>
2025-03-28 19:13:49 +0100

    ♻️ Broken Task up into TaskHeader and TaskBody

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Checkbox } from "@heroui/checkbox";
 import { Form } from "@heroui/form";
-import { useForm, useStore } from "@tanstack/react-form";
+import { useStore } from "@tanstack/react-form";
 import { useMutation } from "@tanstack/react-query";
 import { Input } from "@heroui/input";
 import { Chip } from "@heroui/chip";
@@ -15,6 +15,7 @@ import {
 import PopoverCalendar from "@/components/popover-calendar";
 import { Task as TaskType } from "@/types";
 import { patchTask, deleteTask } from "@/api/tasks";
+import { withForm, useAppForm } from "@/hooks/form";
 
 type TaskProps = {
   task: TaskType;
@@ -27,13 +28,13 @@ type TaskProps = {
 
 export default function Task({
   task,
-  isOpen,
+  isOpen = false,
   onClose,
   onOpen,
   onDelete,
   className = "",
 }: TaskProps) {
-  const { Field, Subscribe, state, store, handleSubmit } = useForm({
+  const form = useAppForm({
     defaultValues: task,
     onSubmit: () => onClose?.(),
   });
@@ -48,12 +49,13 @@ export default function Task({
   });
 
   useEffect(() => {
-    patchMutation.mutateAsync(state.values);
-  }, [useStore(store, ({ values }) => values)]);
+    patchMutation.mutateAsync(form.state.values);
+  }, [useStore(form.store, ({ values }) => values)]);
 
   return (
     <div
-      className={`cursor-auto transition-all duration-200 relative hover:bg-default-100 hover:shadow-lg rounded-xl ${className} ${isOpen ? "bg-default-100 shadow-lg" : ""}`}
+      className={`cursor-auto transition-all duration-200 relative hover:bg-default-100 hover:shadow-lg rounded-xl ${className} 
+        ${isOpen ? "bg-default-100 shadow-lg" : ""}`}
       id={`task-${task.id}`}
       role="button"
       tabIndex={0}
@@ -70,128 +72,146 @@ export default function Task({
         className="flex-col gap-0"
         onSubmit={(e) => {
           e.preventDefault();
-          handleSubmit();
+          form.handleSubmit();
         }}
       >
+        <TaskHeader
+          form={form}
+          isOpen={isOpen}
+          onDelete={() => deleteMutation.mutateAsync(task.id)}
+        />
         <div
-          className={`flex items-center w-full px-2 h-10 ${isOpen ? "" : "cursor-pointer"}`}
+          className={`w-full overflow-hidden transition-all duration-200 
+            ${isOpen ? "max-h-96 opacity-100 ease-in" : "max-h-0 opacity-0"}`}
         >
-          <Field name="done">
-            {({ state, handleChange, handleBlur }) => (
-              <Checkbox
-                className="-mr-1 pr-0"
-                isSelected={state.value}
-                onBlur={handleBlur}
-                onValueChange={(checked) => handleChange(checked)}
-              />
-            )}
-          </Field>
-          {isOpen ? (
-            <Field name="name">
-              {({ state, handleChange, handleBlur }) => (
-                <Input
-                  autoFocus={state.value == ""}
-                  classNames={{
-                    inputWrapper:
-                      "bg-transparent shadow-none data-[hover=true]:bg-transparent",
-                    input: "text-base",
-                  }}
-                  defaultValue={state.value}
-                  placeholder="New To-Do"
-                  size="sm"
-                  type="name"
-                  onBlur={handleBlur}
-                  onChange={(e) => handleChange(e.target.value)}
-                  onKeyDown={(e) => {
-                    if (e.code === "Backspace" && state.value == "")
-                      deleteMutation.mutateAsync(task.id);
-                  }}
-                />
-              )}
-            </Field>
-          ) : (
-            <>
-              <Subscribe selector={(state) => state.values.done}>
-                {(done) => (
-                  <span
-                    className={`ml-2 truncate ${done ? "line-through text-default-400" : ""}`}
-                  >
-                    {state.values.name}
-                  </span>
-                )}
-              </Subscribe>
-              {state.values.deadlineDate && (
-                <Subscribe selector={(state) => state.values.deadlineDate}>
-                  {(deadlineDate) => (
-                    <Chip
-                      className="ml-auto"
-                      color="danger"
-                      startContent={<FlagIconFilled size={18} />}
-                      variant="flat"
-                    >
-                      {deadlineDate.compare(today(getLocalTimeZone()))}d left
-                    </Chip>
-                  )}
-                </Subscribe>
-              )}
-            </>
-          )}
-        </div>
-        <div
-          className={`w-full flex flex-row justify-between overflow-hidden transition-all duration-200 ${isOpen ? "max-h-96 opacity-100 ease-in" : "max-h-0 opacity-0"}`}
-        >
-          {isOpen && (
-            <>
-              <div className="flex flex-col gap-2 pl-2 pb-2">
-                <Subscribe selector={(state) => state.values.dueDate}>
-                  {(dueDate) =>
-                    dueDate ? (
-                      <Chip
-                        radius="sm"
-                        startContent={<CalendarIcon size={18} />}
-                      >
-                        {dueDate.day}. {dueDate.month}.
-                      </Chip>
-                    ) : null
-                  }
-                </Subscribe>
-                <Subscribe selector={(state) => state.values.deadlineDate}>
-                  {(deadlineDate) =>
-                    deadlineDate ? (
-                      <Chip
-                        radius="sm"
-                        startContent={<FlagIconFilled size={18} />}
-                      >
-                        {deadlineDate.day}. {deadlineDate.month}.
-                      </Chip>
-                    ) : null
-                  }
-                </Subscribe>
-              </div>
-              <div className="flex items-end">
-                <Field name="dueDate">
-                  {({ state, handleChange }) => (
-                    <PopoverCalendar
-                      defaultValue={state.value}
-                      trigger={<CalendarIcon size={18} />}
-                      onChange={(value) => handleChange(value)}
-                    />
-                  )}
-                </Field>
-                <Field name="deadlineDate">
-                  {({ state, handleChange }) => (
-                    <PopoverCalendar
-                      defaultValue={state.value}
-                      trigger={<FlagIcon size={18} />}
-                      onChange={(value) => handleChange(value)}
-                    />
-                  )}
-                </Field>
-              </div>
-            </>
-          )}
+          {isOpen && <TaskBody form={form} />}
         </div>
       </Form>
     </div>
   );
 }
+
+const TaskHeader = withForm({
+  defaultValues: {} as TaskType,
+  props: {
+    isOpen: false,
+    onDelete: () => {},
+  },
+  render: function Render({ form, isOpen, onDelete }) {
+    return (
+      <div
+        className={`flex items-center w-full px-2 h-10 ${isOpen ? "" : "cursor-pointer"}`}
+      >
+        <form.Field name="done">
+          {({ state, handleChange, handleBlur }) => (
+            <Checkbox
+              className="-mr-1 pr-0"
+              isSelected={state.value}
+              onBlur={handleBlur}
+              onValueChange={(checked) => handleChange(checked)}
+            />
+          )}
+        </form.Field>
+        {isOpen ? (
+          <form.Field name="name">
+            {({ state, handleChange, handleBlur }) => (
+              <Input
+                autoFocus={state.value == ""}
+                classNames={{
+                  inputWrapper:
+                    "bg-transparent shadow-none data-[hover=true]:bg-transparent",
+                  input: "text-base",
+                }}
+                defaultValue={state.value}
+                placeholder="New To-Do"
+                size="sm"
+                type="name"
+                onBlur={handleBlur}
+                onChange={(e) => handleChange(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.code === "Backspace" && state.value == "") onDelete();
+                }}
+              />
+            )}
+          </form.Field>
+        ) : (
+          <>
+            <form.Subscribe selector={(state) => state.values.done}>
+              {(done) => (
+                <span
+                  className={`ml-2 truncate ${done ? "line-through text-default-400" : ""}`}
+                >
+                  {form.state.values.name}
+                </span>
+              )}
+            </form.Subscribe>
+            {form.state.values.deadlineDate && (
+              <form.Subscribe selector={(state) => state.values.deadlineDate}>
+                {(deadlineDate) => (
+                  <Chip
+                    className="ml-auto"
+                    color="danger"
+                    startContent={<FlagIconFilled size={18} />}
+                    variant="flat"
+                  >
+                    {deadlineDate.compare(today(getLocalTimeZone()))}d left
+                  </Chip>
+                )}
+              </form.Subscribe>
+            )}
+          </>
+        )}
+      </div>
+    );
+  },
+});
+
+const TaskBody = withForm({
+  defaultValues: {} as TaskType,
+  render: function Render({ form }) {
+    return (
+      <div className="w-full flex flex-row justify-between">
+        <div className="flex flex-col gap-2 pl-2 pb-2">
+          <form.Subscribe selector={(state) => state.values.dueDate}>
+            {(dueDate) =>
+              dueDate ? (
+                <Chip radius="sm" startContent={<CalendarIcon size={18} />}>
+                  {dueDate.day}. {dueDate.month}.
+                </Chip>
+              ) : null
+            }
+          </form.Subscribe>
+          <form.Subscribe selector={(state) => state.values.deadlineDate}>
+            {(deadlineDate) =>
+              deadlineDate ? (
+                <Chip radius="sm" startContent={<FlagIconFilled size={18} />}>
+                  {deadlineDate.day}. {deadlineDate.month}.
+                </Chip>
+              ) : null
+            }
+          </form.Subscribe>
+        </div>
+        <div className="flex items-end">
+          <form.Field name="dueDate">
+            {({ state, handleChange }) => (
+              <PopoverCalendar
+                defaultValue={state.value}
+                trigger={<CalendarIcon size={18} />}
+                onChange={(value) => handleChange(value)}
+              />
+            )}
+          </form.Field>
+          <form.Field name="deadlineDate">
+            {({ state, handleChange }) => (
+              <PopoverCalendar
+                defaultValue={state.value}
+                trigger={<FlagIcon size={18} />}
+                onChange={(value) => handleChange(value)}
+              />
+            )}
+          </form.Field>
+        </div>
+      </div>
+    );
+  },
+});
