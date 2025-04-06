09e3f91e9dd20dc7914b44c82d2be722aaca4b30
Vojtěch Pašek <pasekvoj@fit.cvut.cz>
2025-04-06 14:47:35 +0200

    🔧 Added .ts files to the Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ import { heroui } from "@heroui/theme";
 export default {
   content: [
     "./index.html",
-    "./src/**/*.tsx",
+    "./src/**/*.{ts,tsx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
