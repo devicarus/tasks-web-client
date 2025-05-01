9da29dfa18ba746cc4441ec75e44f3e3f5189b8a
Vojtěch Pašek <pasekvoj@fit.cvut.cz>
2025-05-01 15:32:28 +0200

    💄 Added header-sm variant for Input

--- src/shared/components/variants/input.tsx
+++ src/shared/components/variants/input.tsx
@@ -2,8 +2,16 @@ import { extendVariants } from "@heroui/system";
 import { VariantProps } from "tailwind-variants";
 import { Input as HeroInput } from "@heroui/input";
 
+import { title } from "@/shared/components/primitives";
+
 const Input = extendVariants(HeroInput, {
   variants: {
+    size: {
+      "header-sm": {
+        input: title({ size: "sm" }),
+        inputWrapper: "p-0",
+      },
+    },
     variant: {
       plain: {
         inputWrapper:
